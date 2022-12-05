993b3663de5fe18546928a2bcea86a6a57730547
vyusufcan <vehbiyusufcan@gmail.com>
2022-12-05 18:54:48 +0300

    board implementation

--- board.py
+++ board.py
@@ -1,5 +1,3 @@
-from player import Player
-from move import Move
 class Board:
 
     EMPTY_CELL = 0
@@ -37,12 +35,44 @@ class Board:
             self.game_board[row][col] = player.marker
         else:
             print("This position is already taken. Please enter another one")
+    
+    def check_is_game_over(self, player, last_move):
+        return ((self.check_row(player,last_move))
+                 or (self.check_column(player,last_move))
+                 or (self.check_diaginal(player, last_move))
+                 or (self.check_antidiagonal(player,last_move)))
+    
+    def check_row(self, player, last_move):
+        row_index = last_move.get_row()
+        board_row = self.game_board[row_index] # ["0", "0", "X"]
 
-board = Board()
-board.print_board()
+        return board_row.count(player.marker) == 3
 
-player = Player()
-move = Move(5)
-board.submit_move(player, move)
-board.print_board()
+    def check_column(self, player, last_move):
+        markers_count = 0
+        column_index = last_move.get_column()
+
+        for i in range(3):
+            if self.game_board[i][column_index] == player.marker:
+                markers_count += 1
+        
+        return markers_count == 3
+    
+    def check_diagonal(self, player):
+        markers_count = 0
+
+        for i in range(3):
+            if self.game_board[i][i] == player.marker:
+                markers_count += 1
+
+        return markers_count == 3
+    
+    def check_antidiagonal(self, player):
+        markers_count = 0
+        
+        for i in range(3):
+            if self.game_board[i][2-i] == player.marker:
+                markers_count += 1
+        
+        return markers_count == 3
     
